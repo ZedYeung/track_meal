711e7bbe39b1f9f265ca369447477de1fc1d29ee
Yue Yang <shoominga@gmail.com>
2017-11-24 22:59:12 -0800

    feat: Create redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import reducer from './reducers'
+
+const store = createStore(reducer)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
